a127c7a43607c61a39a3d2f3a22324b7f5d6e539
bguerschom <148221717+bguerschom@users.noreply.github.com>
2024-11-20 11:50:28 +0200

    handle the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import LoginPage from './components/LoginPage'
+import React, { useState } from 'react';
+import LoginPage from './components/LoginPage';
+import WelcomePage from './components/WelcomePage';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
+
+  const handleLoginSuccess = (user) => {
+    setUsername(user);
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+  };
+
   return (
-    <LoginPage />
-  )
+    <>
+      {!isLoggedIn ? (
+        <LoginPage onLoginSuccess={handleLoginSuccess} />
+      ) : (
+        <WelcomePage username={username} onLogout={handleLogout} />
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
